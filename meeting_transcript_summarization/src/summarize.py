bcc0931f1d5a2c681869381e2993a38be01a87b1
Siddharth Soni <sonisiddharth2003@gmail.com>
2024-07-01 15:12:19 +0530

    [+] completed pipeline for summarization

--- meeting_transcript_summarization/src/summarize.py
+++ meeting_transcript_summarization/src/summarize.py
@@ -1,3 +1,18 @@
+from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM
+
+
+class Summarizer:
+    def __init__(self, model_dir):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_dir)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
+
+    def summarize(self, text, max_new_tokens=100):
+        inputs = self.tokenizer(text, return_tensors="pt").input_ids
+        outputs = self.model.generate(inputs, max_new_tokens=max_new_tokens, do_sample=False)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+
 def read_transcript(file_path):
     with open(file_path, 'r') as file:
         transcript = file.read()
